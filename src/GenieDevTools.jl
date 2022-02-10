088724bcf1700a4a6f3bd0de18b18737b1bd33be
essenciary <e@essenciary.com>
2022-02-10 19:51:03 +0100

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.26

---
+++ src/GenieDevTools.jl
@@ -0,0 +1,5 @@
+module GenieDevTools
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using GenieDevTools
+using Test
+
+@testset "GenieDevTools.jl" begin
+    # Write your tests here.
+end
