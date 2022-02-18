791f1e6cab63fd4e5dd02528192c17fd8b94046e
essenciary <e@essenciary.com>
2022-02-18 19:34:52 +0100

    initial commit

--- src/GenieDevTools.jl
+++ src/GenieDevTools.jl
@@ -1,5 +1,30 @@
 module GenieDevTools
 
-# Write your package code here.
+using Genie
+using Genie.Renderers.Json, Genie.Renderers.Html
+
+using Revise
+
+const defaultroute = "/_devtools_"
+
+function register_routes(defaultroute = defaultroute)
+  route("$defaultroute/errors") do
+    Revise.queue_errors |> json
+  end
+
+  route("$defaultroute/fs") do
+    walkdir(pwd()) |> collect |> json
+  end
+
+  routes("$defaultroute/exec/:command", method = [GET, POST]) do
+    Core.eval(Main, Meta.parse(params(:command))) |> html
+  end
+
+  route("$defaultroute/id") do
+    (id => Main.UserApp) |> json
+  end
+
+  nothing
+end
 
 end
