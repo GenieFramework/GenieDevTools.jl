dce7759c47ec729f944cf158a87aca91707be27e
Adrian Salceanu <adrian.salceanu@gmail.com>
2023-04-10 21:33:26 +0200

    Fix model name

--- src/route_helpers.jl
+++ src/route_helpers.jl
@@ -174,7 +174,7 @@ function pages(defaultroute)
       page_info = Dict(
         :route => Dict(:method => p.route.method, :path => p.route.path),
         :view => p.view |> string,
-        :model => Dict( :name => Genie.Generator.validname(p.model |> string),
+        :model => Dict( :name => Genie.Generator.validname(typeof(p.model)),
                         :fields => modelfieldsinfo(instance)),
         :layout => length(p.layout) < Stipple.IF_ITS_THAT_LONG_IT_CANT_BE_A_FILENAME && isfile(p.layout) ? p.layout : nothing,
         :deps => modeldeps(instance),
