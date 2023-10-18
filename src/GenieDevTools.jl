14a95b19c85a87d225b2ff3106be4e37ec772127
Adrian Salceanu <adrian.salceanu@gmail.com>
2023-10-18 21:43:21 +0200

    Log watching features

--- src/GenieDevTools.jl
+++ src/GenieDevTools.jl
@@ -25,6 +25,78 @@ function runhooks()
   end
 end
 
+function tailapplog(handler::Function, logdirpath::String; frequency::Float64 = 0.5, env::AbstractString = "dev")
+  logpath = joinpath(logdirpath, "$env-$(Dates.today()).log")
+  if ! isfile(logpath)
+    @error "No log file found at $logpath"
+    return
+  end
+
+  open(logpath) do io
+    if ! isreadable(io)
+      @error "Log file at $logpath is not readable"
+      return
+    end
+
+    seekend(io)
+
+    while true
+      line = read(io, String)
+      if ! isempty(line)
+        handler(line)
+      end
+      sleep(frequency)
+    end
+
+    @info "Finished watching log file at $logpath"
+    close(io)
+  end
+end
+
+function logtype(line::String) :: Symbol
+  if startswith(line, "┌ ") # start of log line
+    if startswith(line, "┌ Debug: ")
+      return :debug
+    elseif startswith(line, "┌ Warn: ")
+      return :warn
+    elseif startswith(line, "┌ Error: ")
+      return :error
+    end
+  end
+
+  return :info
+end
+
+function parselog(line::AbstractString) :: Union{AbstractString,Nothing}
+  # check for missing packages
+  m = match(r"ArgumentError: Package (\w*) not found", line)
+  if ! isnothing(m) && length(m.captures) >= 1
+    return "log:critical:package_not_found $(m.captures[1])"
+  end
+
+  # catch revise errors
+  r = r"""The running code does not match the saved version for the following files:
+  (.*)
+  (.*)"""
+  m = match(r, line)
+  if ! isnothing(m) && length(m.captures) >= 2
+    return "log:critical:revise_error $(m.captures[2])"
+  end
+
+  # catch parse errors
+  r = r"""LoadError: ParseError:
+  (.*)# Error @ (.*)
+  (.*)
+  (.*)
+  (.*)"""
+  m = match(r, line)
+  if ! isnothing(m) && length(m.captures) >= 5
+    return "log:critical:parse_error $(m.captures[2])"
+  end
+
+  return
+end
+
 function register_routes(defaultroute = defaultroute) :: Nothing
   RH.errors(defaultroute)
   RH.dir(defaultroute)
